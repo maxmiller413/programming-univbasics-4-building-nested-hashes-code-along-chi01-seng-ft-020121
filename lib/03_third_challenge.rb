489b6cb64ad074f01aa5af39b2c121bfa26b5792
Max Miller <maxmiller413@gmail.com>
2021-01-15 02:39:44 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,32 +2,15 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {},
-      :matriarch => {
-        :name => "Lady Montague", 
-        :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15",
-        :status => "alive"
-        },
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
         
    }, 
    :capulet => {
-      :patriarch => {
-        :name => "Lord Capulet",
-        :age => "50"
-        },
-      :matriarch => {
-        :name => "Lady Capulet",
-        :age => "51"
-        },
-      :heroine => {
-        :name => "Juliet",
-        :age => "15",
-        :status => "alive"
-        },
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
    }
   }
